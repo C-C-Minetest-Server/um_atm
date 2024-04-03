d2793f390b308ba692e773c57ef1341e13a3a9c4
1F616EMO <root@1f616emo.xyz>
2024-04-03 08:52:36 +0800

    UI change

--- gui.lua
+++ gui.lua
@@ -29,7 +29,7 @@ local function handle_currency_take(t, amount)
         end
 
         local inv = player:get_inventory()
-        local currency_name = (t ~= 1) and ("currency:minegeld_" .. str(t)) or "currency:minegeld"
+        local currency_name = enabled_count[t]
         local stack = ItemStack(currency_name .. " " .. str(amount))
         if not inv:room_for_item("main", stack) then
             minetest.chat_send_player(name, S("Not enough room in your inventory"))
@@ -51,7 +51,7 @@ local function handle_currency_add(t, amount)
         local name = player:get_player_name()
         local inv = player:get_inventory()
 
-        local currency_name = (t ~= 1) and ("currency:minegeld_" .. str(t)) or "currency:minegeld"
+        local currency_name = enabled_count[t]
         local stack = ItemStack(currency_name .. " " .. str(amount))
         if not inv:contains_item("main", stack) then
             minetest.chat_send_player(name, S("Not enough money in your inventory"))
@@ -71,145 +71,127 @@ local function handle_currency_add(t, amount)
     end
 end
 
+local function handle_currency_action(t, amount)
+    if amount < 0 then
+        return handle_currency_add(t, -amount)
+    end
+    return handle_currency_take(t, amount)
+end
+
+local function handle_deposit_all(t)
+    return function(player, ctx)
+        local name = player:get_player_name()
+        local inv = player:get_inventory()
+        local currency_name = enabled_count[t]
+
+        local stack = inv:remove_item("main", currency_name .. " 65535")
+        local amount = stack:get_count() * enabled_key[currency_name]
+
+        unified_money.add_balance(name, amount)
+        return true
+    end
+end
+
+local function handle_withdraw_all(t)
+    return function(player, ctx)
+        local name = player:get_player_name()
+        local inv = player:get_inventory()
+        local balance = unified_money.get_balance_safe(name)
+
+        local take = math.floor(balance / t)
+        balance = balance % t
+
+        local stack = ItemStack(enabled_count[t])
+        stack:set_count(take)
+        stack = inv:add_item("main", stack)
+        balance = balance + (stack:get_count() * t)
+
+        unified_money.set_balance_safe(name, balance)
+
+        return true
+    end
+end
+
+local function count_item(player, t)
+    local itemname = enabled_count[t]
+    local inv = player:get_inventory()
+    local count = 0
+
+    for _, stack in ipairs(inv:get_list("main")) do
+        if stack:get_name() == itemname then
+            count = count + stack:get_count()
+        end
+    end
+    return count
+end
+
+local function signed(num)
+    return (num < 0 and "-" or "+") .. tostring(math.abs(num))
+end
+
+local columns_lvl = {
+    [1] = { -1, 1 },
+    [2] = { -10, -1, 1, 10, },
+    [3] = { false, -100, -10, -1, 1, 10, 100, true }
+}
+
+local insert = table.insert
 
 um_atm.gui = flow.make_gui(function(player, ctx)
     local name = player:get_player_name()
     local balance = unified_money.get_balance_safe(name)
     ctx.lvl = ctx.lvl or 1
 
-    local header = gui.Stack {
+    local columns = columns_lvl[ctx.lvl] or columns_lvl[1]
+    local body = {} -- VBox
+
+    insert(body, gui.Stack {
         gui.Label {
             align_h = "center", expand = true,
-            label = um_translate_common.balance_show(balance),
+            label = S("Deposit / Withdraw"),
         },
         gui.ButtonExit {
             align_h = "right", expand = true,
             w = 0.7, h = 0.7, label = "x"
         }
-    }
+    })
 
-    local amounts = { 1 }
-    if ctx.lvl >= 2 then
-        table.insert(amounts, 10)
-        if ctx.lvl >= 3 then
-            table.insert(amounts, 100)
-        end
-    end
-
-    local body = {}    -- Hbox or VBox depends on lvl
-
-    do                 -- Money input
-        local lbox = { -- VBox
-            gui.Label {
-                align_h = "center",
-                label = S("Money input"),
-            },
-        }
-        do
-            local col1 = {} -- HBox
-            for _, n in ipairs(enabled_currency) do
-                table.insert(col1, gui.ItemImage {
-                    w = 1, h = 1,
-                    item_name = (n ~= 1) and ("currency:minegeld_" .. str(n)) or "currency:minegeld"
-                })
-            end
-            table.insert(lbox, gui.Hbox(col1))
-        end
-        for _, a in ipairs(amounts) do
-            local col = {} -- HBox
-            for _, n in ipairs(enabled_currency) do
-                table.insert(col, gui.Button {
-                    w = 1, h = 1,
-                    label = str(a),
-                    on_event = handle_currency_add(n, a)
-                })
-            end
-            table.insert(lbox, gui.Hbox(col))
-        end
-        table.insert(lbox, gui.Button {
-            w = 1, h = 1,
-            label = S("Inventory to ATM"),
-            on_event = function(player, ctx)
-                local name = player:get_player_name()
-                local inv = player:get_inventory()
-
-                local main_size = inv:get_size("main")
-                for i = 1, main_size do
-                    local stack = inv:get_stack("main", i)
-                    if enabled_key[stack:get_name()] then
-                        local count = stack:get_count()
-                        local amount = count * enabled_key[stack:get_name()]
-
-                        unified_money.add_balance(name, amount)
-                        inv:set_stack("main", i, ItemStack())
-                    end
+    for _, money in ipairs(enabled_currency) do
+        if ctx.lvl > 1 or money <= 10 then
+            local row = {} -- HBox
+            insert(row, gui.ItemImage {
+                w = 1, h = 1,
+                item_name = (money ~= 1) and ("currency:minegeld_" .. str(money)) or "currency:minegeld"
+            })
+            for _, amount in ipairs(columns) do
+                if type(amount) == "boolean" then
+                    insert(row, gui.Button {
+                        w = 1, h = 1,
+                        label = (amount and "+" or "-") .. S("ALL"),
+                        on_event = (amount and handle_withdraw_all or handle_deposit_all)(money)
+                    })
+                else
+                    insert(row, gui.Button {
+                        w = 1, h = 1,
+                        label = signed(amount),
+                        on_event = handle_currency_action(money, amount)
+                    })
                 end
-                return true
             end
-        })
-        table.insert(body, gui.Vbox(lbox))
+
+            insert(row, gui.Label {
+                w = 1, h = 1,
+                label = S("You have:@n@1", count_item(player, money))
+            })
+
+            insert(body, gui.HBox(row))
+        end
     end
 
-    table.insert(body, gui.Box { w = 0.05, h = 0.05, color = "grey", padding = 0 })
+    insert(body, gui.Label {
+        align_h = "right",
+        label = um_translate_common.balance_show(balance)
+    })
 
-    do                 -- Money output
-        local rbox = { -- VBox
-            gui.Label {
-                align_h = "center",
-                label = S("Money output"),
-            },
-        }
-        do
-            local col1 = {} -- HBox
-            for _, n in ipairs(enabled_currency) do
-                table.insert(col1, gui.ItemImage {
-                    w = 1, h = 1,
-                    item_name = (n ~= 1) and ("currency:minegeld_" .. str(n)) or "currency:minegeld"
-                })
-            end
-            table.insert(rbox, gui.Hbox(col1))
-        end
-        for _, a in ipairs(amounts) do
-            local col = {} -- HBox
-            for _, n in ipairs(enabled_currency) do
-                table.insert(col, gui.Button {
-                    w = 1, h = 1,
-                    label = str(a),
-                    on_event = handle_currency_take(n, a)
-                })
-            end
-            table.insert(rbox, gui.Hbox(col))
-        end
-        table.insert(rbox, gui.Button {
-            w = 1, h = 1,
-            label = S("ATM to Inventory"),
-            on_event = function(player, ctx)
-                local name = player:get_player_name()
-                local inv = player:get_inventory()
-                local balance = unified_money.get_balance_safe(name)
-
-                for _, n in ipairs({ 100, 50, 10, 5, 1 }) do
-                    if enabled_count[n] then
-                        local take = math.floor(balance / n)
-                        balance = balance % n
-
-                        local stack = ItemStack(enabled_count[n])
-                        stack:set_count(take)
-                        stack = inv:add_item("main", stack)
-                        balance = balance + (stack:get_count() * n)
-                    end
-                end
-
-                unified_money.set_balance_safe(name, balance)
-
-                return true
-            end
-        })
-        table.insert(body, gui.Vbox(rbox))
-    end
-
-    return gui.VBox {
-        header,
-        ((ctx.lvl == 1) and gui.VBox or gui.HBox)(body)
-    }
+    return gui.VBox(body)
 end)
